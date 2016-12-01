a0154957d9e173a0b38acbcba10b0dc57f372d57
Michael Hearne <mhearne@usgs.gov>
2016-12-01 16:52:13 -0700

    Streamlined query module by making it an object that only contacts comcat once for any given event.

--- impactutils/comcat/query.py
+++ impactutils/comcat/query.py
@@ -4,21 +4,50 @@ import json
 
 URL_TEMPLATE = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/detail/[EVENTID].geojson'
 
-def get_associated_ids(eventid):
-    """Query ComCat for the event IDs associated with input ID.
+class ComCatInfo(object):
+    def __init__(self,eventid):
+        url = URL_TEMPLATE.replace('[EVENTID]',eventid)
+        try:
+            fh = request.urlopen(url)
+            data = fh.read().decode('utf-8')
+            fh.close()
+            self._jdict = json.loads(data)
+        except:
+            raise PagerException('Could not connect to ComCat server.')
 
-    :param eventid:
-      Input eventid to be passed to ComCat.
-    :returns:
-      Tuple of (authoritative event ID, all other IDS associated with event)
-    """
-    url = URL_TEMPLATE.replace('[EVENTID]',eventid)
-    fh = request.urlopen(url)
-    data = fh.read().decode('utf-8')
-    fh.close()
-    jdict = json.loads(data)
-    allids = jdict['properties']['ids'].strip(',').split(',')
-    authid = jdict['id']
-    allids.remove(authid)
-    return (authid,allids)
-    
+    def getAssociatedIds(self):
+        """Query ComCat for the event IDs associated with input ID.
+
+        :param eventid:
+          Input eventid to be passed to ComCat.
+        :returns:
+          Tuple of (authoritative event ID, all other IDS associated with event) or (None,None) 
+          if url cannot be reached.
+        """
+        allids = self._jdict['properties']['ids'].strip(',').split(',')
+        authid = self._jdict['id']
+        allids.remove(authid)
+        return (authid,allids)
+
+    def getLocation(self):
+        """Query ComCat for the location string associated with input ID.
+
+        :param eventid:
+          Input eventid to be passed to ComCat.
+        :returns:
+          Location string (i.e. "24km NE of Dharchula, India")
+        """
+        location = self._jdict['properties']['place']
+        return location
+
+    def getTsunami(self):
+        """Query ComCat for the location string associated with input ID.
+
+        :param eventid:
+          Input eventid to be passed to ComCat.
+        :returns:
+          Location string (i.e. "24km NE of Dharchula, India")
+        """
+        tsunami = self._jdict['properties']['tsunami']
+        return tsunami
+
