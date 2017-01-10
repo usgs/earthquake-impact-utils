a116ee453772fb806fda93cb2286a449abf6e4c2
Michael Hearne <mhearne@usgs.gov>
2017-01-10 11:42:25 -0700

    Modified code that handles determination of local time - now handles dst.

--- test/time/timeutils_test.py
+++ test/time/timeutils_test.py
@@ -3,29 +3,61 @@
 #stdlib imports
 import os.path
 import sys
-from collections import OrderedDict
 from datetime import datetime,timedelta
+from collections import OrderedDict
 
 #hack the path so that I can debug these functions if I need to
 homedir = os.path.dirname(os.path.abspath(__file__)) #where is this script?
 impactdir = os.path.abspath(os.path.join(homedir,'..','..'))
 sys.path.insert(0,impactdir) #put this at the front of the system path, ignoring any installed impact stuff
 
-from impactutils.time.timeutils import get_local_time,ElapsedTime
+from impactutils.time.timeutils import LocalTime,ElapsedTime
 
 def local_time_test():
-    offsets = {'Manhattan':(40.7831, -73.9712,-5),
-               'Denver':(39.704545,-104.941406,-7),
-               'LA':(33.864714,-118.212891,-8)}
-
+    
+    standard_offsets = OrderedDict([('Manhattan',(40.7831, -73.9712,-5)),
+                                    ('Denver',(39.704545,-104.941406,-7)),
+                                    ('LA',(33.864714,-118.212891,-8))])
     utctime = datetime(2016,1,1,20,23,00)
-    for key,value in offsets.items():
+    ltime = None
+    for key,value in standard_offsets.items():
         lat,lon,cmpoffset = value
-        localtime = get_local_time(utctime,lat,lon)
+        t1 = datetime.now()
+        if ltime is None:
+            ltime = LocalTime(utctime,lat,lon)
+        else:
+            ltime.update(utctime,lat,lon)
+        t2 = datetime.now()
+        dt = t2 - t1
+        print('Testing standard time offset for %s' % key)
+        localtime = ltime.getLocalTime()
+        seconds = dt.seconds + dt.microseconds/1e6
         cmptime = utctime + timedelta(hours=cmpoffset)
-        print('Testing time offset for %s' % key)
         assert localtime == cmptime
-        print('Time offset correct.')
+        print('Time offset correct - %.1f seconds.' % seconds)
+
+    dst_offsets = OrderedDict([('Manhattan',(40.7831, -73.9712,-4)),
+                               ('Denver',(39.704545,-104.941406,-6)),
+                               ('Phoenix',(33.421556,-112.06604,-7)),
+                               ('LA',(33.864714,-118.212891,-7))])
+
+    utctime = datetime(2016,8,1,20,23,00)
+    ltime = None
+    for key,value in dst_offsets.items():
+        print('Testing DST time offset for %s' % key)
+        lat,lon,cmpoffset = value
+        t1 = datetime.now()
+        if ltime is None:
+            ltime = LocalTime(utctime,lat,lon)
+        else:
+            ltime.update(utctime,lat,lon)
+        t2 = datetime.now()
+        dt = t2 - t1
+        localtime = ltime.getLocalTime()
+        seconds = dt.seconds + dt.microseconds/1e6
+        cmptime = utctime + timedelta(hours=cmpoffset)
+        assert localtime == cmptime
+        print('Time offset correct - %.1f seconds.' % seconds)
 
 def elapsed_test():
     etime = ElapsedTime()
@@ -47,6 +79,8 @@ def elapsed_test():
         assert compstr == estr
 
 if __name__ == '__main__':
-    elapsed_test()
     local_time_test()
+    elapsed_test()
     
+    
+
